c3501051bb18ff33063bcaf7c78d8908c0d569c6
pbannavong <Phillip.Bannavong@gmail.com>
2020-09-09 15:54:47 -0700

    updated actions index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
+  dispatch({ type: "FETCH_POST", payload: response });
+};
+
+export const selectPost = () => {
   return {
-    type: "FETCH_POSTS",
-    payload: response,
+    type: "SELECT_POST",
   };
 };
