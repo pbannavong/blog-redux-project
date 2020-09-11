86b01105b5f91d4ba4bab8439aae6b05263844ce
pbannavong <Phillip.Bannavong@gmail.com>
2020-09-10 20:28:57 -0700

    refactored fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+  // refactored below
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
